dcc9ec55956aee486f7802064ec68d31547ce291
Vanya Startsev <vanya.startsev.60@mail.ru>
2022-02-17 23:02:03 +0500

    3rd

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, url_for, request, render_template
+import json
 app = Flask(__name__)
 
 
@@ -17,6 +18,18 @@ def training(prof):
     return render_template('training.html', image=sxema[1], text=sxema[0])
 
 
+@app.route('/list_prof/<list>')
+def show_list(list): # незнаю как указать путь до json файла, помогите пожалуйста
+    tag = 0
+    if list == 'ol':
+        tag = 1
+    elif list == 'ul':
+        tag = 2
+    with open("C:/Users/vakva/PycharmProjects/flask_1/static/js/list.json", "rt", encoding="utf8") as f:
+        news_list = json.loads(f.read())
+    return render_template('list.html', news=news_list, tag=tag)
+
+        
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
 
